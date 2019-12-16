f54a2f2354c5e46011dbe783273e06d9584ccaa5
Stranger488 <iilluuhhaaxxdd@gmail.com>
2019-12-17 02:18:24 +0300

    modals every

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,62 @@
-var modal = document.querySelector(".modal-container");
-var modalGuts = document.querySelector(".modal");
-var modalGutsReally = document.querySelector(".modal-guts");
-var modalOverlay = document.querySelector(".modal-overlay");
-var closeButton = document.querySelector("#close-button");
-var openButton = document.querySelector("#open-modal-id");
-var confirmButton = document.querySelector("#confirm-button");
+let modal = document.querySelector(".modal-container");
+let modalGuts = document.querySelector(".modal");
+let modalGutsReally = document.querySelector(".modal-guts");
+let modalOverlay = document.querySelector(".modal-overlay");
+let closeButton = document.querySelector("#close-button");
+let confirmButton = document.querySelector("#confirm-button");
 
+let modalGutsHead = document.querySelector(".modal-guts-head");
+let modalGutsContent = document.querySelector(".modal-guts-content");
 
-if (openButton) {
-    var openButtonHref = openButton.getAttribute("href");
+let allOpenButtons = document.querySelectorAll(".open-modal");
 
-    confirmButton.addEventListener("click", function (event) {
-        window.location.href = openButtonHref;
-    });
+let openInput = document.querySelector(".open-modal-input");
+let openA = document.querySelector(".open-modal-a");
+
+let formSubmit = document.getElementById("form-submit-id");
+console.log(formSubmit);
+
+if (openInput) {
+    openInput.addEventListener("click", function(event) {
+        if (!openInput.classList.contains("inp-active")) {
+            if (openInput.tagName === "INPUT") {
+                modalGutsHead.innerHTML = "Подтверждение действия";
+                modalGutsContent.innerHTML ="Это действие может привести к изменениям в базе данных. Вы точно желаете продолжить?";
+                confirmButton.innerHTML = "Продолжить";
+        
+                confirmButton.addEventListener("click", function (event) {
+                    formSubmit.click();
+                });
+            }
     
-    openButton.addEventListener("click", function(event) {
+            event.preventDefault();
+            formSubmit.classList.toggle("inp-active");
+            modalGuts.classList.toggle("modal-guts-active");
+            modalGutsReally.classList.toggle("modal-guts-active");
+            modalOverlay.classList.toggle("modal-overlay-active");
+        }
+    });
+}
+
+if (openA) {
+    openA.addEventListener("click", function(event) {
+        if (openA.tagName === "A") {
+            modalGutsHead.innerHTML = "Подтверждение выхода";
+            modalGutsContent.innerHTML ="Вы уверены, что хотите выйти?";
+            confirmButton.innerHTML = "Выход";
+    
+            let allOpenButtonsHref = openA.getAttribute("href");
+    
+            confirmButton.addEventListener("click", function (event) {
+                window.location.href = allOpenButtonsHref;
+            });
+        }
+    
+        if (formSubmit) {
+            formSubmit.classList.toggle("inp-active");
+        }
+
         event.preventDefault();
-        modalGuts.classList.toggle("modal-guts-active");
-        modalGutsReally.classList.toggle("modal-guts-active");
-        modalOverlay.classList.toggle("modal-overlay-active");
-    });
-    
-    closeButton.addEventListener("click", function (event) {
         modalGuts.classList.toggle("modal-guts-active");
         modalGutsReally.classList.toggle("modal-guts-active");
         modalOverlay.classList.toggle("modal-overlay-active");
@@ -29,3 +64,14 @@ if (openButton) {
 }
 
 
+if (closeButton) {
+    closeButton.addEventListener("click", function (event) {
+        if (formSubmit) {
+            formSubmit.classList.toggle("inp-active");
+        }
+        modalGuts.classList.toggle("modal-guts-active");
+        modalGutsReally.classList.toggle("modal-guts-active");
+        modalOverlay.classList.toggle("modal-overlay-active");
+    });
+    
+}
